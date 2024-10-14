f08a9c42f743b14d02e4d2125bcc8cc6bb6e539c
Tarkleigh <Fabian.V.Roth@gmail.com>
2024-10-14 11:17:45 +0200

    Adds tests for convert_found_data_to_csv method

--- Testing.py
+++ Testing.py
@@ -34,6 +34,61 @@ class ParserTests(unittest.TestCase):
         qualified_name = CodeFinder.get_fully_qualified_name(current_path)
         self.assertEqual(current_path, qualified_name)
 
+    def test_convert_found_data_to_csv(self):
+        data_to_concert = dict()
+        source_label = "Cardgen"
+        target_label = "Codefinder"
+
+        first_dependency = "XMLParser"
+        second_dependency = "CardGen"
+
+        first_usage = "Main"
+        second_usage = "InputReader"
+        third_usage = "OutputWriter"
+
+        data_to_concert[first_dependency] = [first_usage, second_usage]
+        data_to_concert[second_dependency] = [third_usage]
+
+        converted_data = CodeFinder.convert_found_data_to_csv(data_to_concert, source_label, target_label)
+
+        self.assertEqual(4, len(converted_data))
+
+        # Table header will be covered by another test method
+        first_data_line = converted_data[1]
+        self.assertEqual(first_data_line[0], source_label)
+        self.assertEqual(first_data_line[1], first_dependency)
+        self.assertEqual(first_data_line[2], target_label)
+        self.assertEqual(first_data_line[3], first_usage)
+
+        second_data_line = converted_data[2]
+
+        self.assertEqual(second_data_line[0], source_label)
+        self.assertEqual(second_data_line[1], first_dependency)
+        self.assertEqual(second_data_line[2], target_label)
+        self.assertEqual(second_data_line[3], second_usage)
+
+        third_data_line = converted_data[3]
+
+        self.assertEqual(third_data_line[0], source_label)
+        self.assertEqual(third_data_line[1], second_dependency)
+        self.assertEqual(third_data_line[2], target_label)
+        self.assertEqual(third_data_line[3], third_usage)
+
+    def test_convert_found_data_to_csv_with_empty_dict(self):
+        data_to_concert = dict()
+
+        source_label = "Cardgen"
+        target_label = "Codefinder"
+
+        converted_data = CodeFinder.convert_found_data_to_csv(data_to_concert, source_label, target_label)
+
+        self.assertEqual(1, len(converted_data))
+
+        header_line = converted_data[0]
+        self.assertEqual(header_line[0], "Source Module")
+        self.assertEqual(header_line[1], "Used Class")
+        self.assertEqual(header_line[2], "Target Module")
+        self.assertEqual(header_line[3], "Consuming Class")
 
 if __name__ == '__main__':
     unittest.main()
