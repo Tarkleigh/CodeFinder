a8eea83a2991a075eddea5a8033f6fa86bcdc34a
Tarkleigh <Fabian.V.Roth@gmail.com>
2025-01-22 18:42:57 +0100

    Fixes linter warnings in testing.py

--- testing.py
+++ testing.py
@@ -1,3 +1,4 @@
+"""Contains unit tests for code_finder.py."""
 import os
 import unittest
 import code_finder
@@ -7,32 +8,38 @@ class ParserTests(unittest.TestCase):
     """Test class for code_finder.py."""
 
     def test_find_label_with_src_folder(self):
+        """Test label finding with Gradle-style path"""
         test_directory_path = "finder" + os.sep + "src" + os.sep + "java"
         label = code_finder.find_label(test_directory_path)
         self.assertEqual("finder", label)
 
     def test_find_label_without_src_folder(self):
+        """Test label finding with regular path"""
         test_directory_path = "finder" + os.sep + "main" + os.sep + "java"
         label = code_finder.find_label(test_directory_path)
         self.assertEqual("java", label)
 
     def test_find_label_without_separator(self):
+        """Test label finding with single directory"""
         test_directory_path = "finder"
         label = code_finder.find_label(test_directory_path)
         self.assertEqual("finder", label)
 
     def test_find_label_without_folder_above_src(self):
+        """Test label finding with low entry-level"""
         test_directory_path = "src" + os.sep + "java"
         label = code_finder.find_label(test_directory_path)
         self.assertEqual("src", label)
 
     def test_find_label_without_folder_above_src_and_no_sub_directory(self):
+        """Test label finding with isolated src folder"""
         test_directory_path = "src"
         label = code_finder.find_label(test_directory_path)
         self.assertEqual("src", label)
 
     def test_convert_found_data_to_csv(self):
-        data_to_concert = dict()
+        """Test conversion of found data to CSV"""
+        data_to_concert = {}
         source_label = "Cardgen"
         target_label = "code_finder"
 
@@ -46,8 +53,8 @@ class ParserTests(unittest.TestCase):
         data_to_concert[first_dependency] = [first_usage, second_usage]
         data_to_concert[second_dependency] = [third_usage]
 
-        converted_data = code_finder.convert_found_data_to_csv(data_to_concert, source_label, target_label)
-
+        converted_data = code_finder.convert_found_data_to_csv(data_to_concert, source_label,
+                                                               target_label)
         self.assertEqual(4, len(converted_data))
 
         # Table header will be covered by another test method
@@ -72,12 +79,14 @@ class ParserTests(unittest.TestCase):
         self.assertEqual(third_data_line[3], third_usage)
 
     def test_convert_found_data_to_csv_with_empty_dict(self):
-        data_to_concert = dict()
+        """Test conversion of data to CSV with no usages found"""
+        data_to_concert = {}
 
         source_label = "Cardgen"
         target_label = "code_finder"
 
-        converted_data = code_finder.convert_found_data_to_csv(data_to_concert, source_label, target_label)
+        converted_data = code_finder.convert_found_data_to_csv(data_to_concert, source_label,
+                                                               target_label)
 
         self.assertEqual(1, len(converted_data))
 
@@ -88,6 +97,7 @@ class ParserTests(unittest.TestCase):
         self.assertEqual(header_line[3], "Consuming Class")
 
     def test_search_source_code_for_dependency_usages(self):
+        """Test finding usages of dependencies in the source code"""
         first_dependency = "tarkleigh.code_finder.code_finder"
         second_dependency = "tarkleigh.code_finder.DataFormatter"
         file = "CardGenerator" + os.extsep + "java"
@@ -96,17 +106,13 @@ class ParserTests(unittest.TestCase):
         possible_dependencies.add(first_dependency)
         possible_dependencies.add(second_dependency)
 
-        usages = dict()
-        usages[second_dependency] = ["tarkleigh.coding.cardgen.XMLParser"]
+        usages = {second_dependency: ["tarkleigh.coding.cardgen.XMLParser"]}
 
-        source_code = list()
-        source_code.append("package tarkleigh.coding.cardgen" + os.linesep)
-        source_code.append(os.linesep)
-        source_code.append("import java.util.List;" + os.linesep)
-        source_code.append("import " + first_dependency + ";" + os.linesep)
-        source_code.append("import " + second_dependency + ";" + os.linesep)
-        source_code.append(os.linesep)
-        source_code.append("class CardGenerator")
+        source_code = ["package tarkleigh.coding.cardgen" + os.linesep, os.linesep,
+                       "import java.util.List;" + os.linesep,
+                       "import " + first_dependency + ";" + os.linesep,
+                       "import " + second_dependency + ";" + os.linesep, os.linesep,
+                       "class CardGenerator"]
 
         code_finder.search_source_code_for_dependency_usages(file, source_code,
                                                              possible_dependencies, usages)
@@ -122,29 +128,32 @@ class ParserTests(unittest.TestCase):
         self.assertEqual("tarkleigh.coding.cardgen.CardGenerator", second_usage_second_dependency)
 
     def test_extract_class_name(self):
+        """Test extracting the class name from a Java file"""
         test_file_name = "CardGenerator" + os.extsep + "java"
         label = code_finder.extract_class_name(test_file_name)
         self.assertEqual("CardGenerator", label)
 
     def test_extract_class_name_with_wrong_file_type(self):
+        """Test extracting the class name from invalid file"""
         test_file_name = "CardGenerator" + os.extsep + "py"
         label = code_finder.extract_class_name(test_file_name)
         self.assertEqual(test_file_name, label)
 
     def test_search_line_for_package_name(self):
+        """Test extracting the package name from a line of source code"""
         # Using a somewhat usual formatting to test all cases
         test_line = "  package" + " org.tarkleigh.foundation" + " ;"
         package_name = code_finder.search_line_for_package_name(test_line, 2)
         self.assertEqual("org.tarkleigh.foundation", package_name)
 
     def test_search_line_unix_line_ending_handling(self):
-        # Using a somewhat usual formatting to test all cases
+        """Test extracting the package name from a line of source code with unix line handling"""
         test_line = "package" + "org.tarkleigh.foundation" + ";\r"
         package_name = code_finder.search_line_for_package_name(test_line, 0)
         self.assertEqual("org.tarkleigh.foundation", package_name)
 
     def test_search_line_windows_line_ending_handling(self):
-        # Using a somewhat usual formatting to test all cases
+        """Test extracting the package name from a line of source code with Windows line handling"""
         test_line = "package" + "org.tarkleigh.foundation" + ";\r\n"
         package_name = code_finder.search_line_for_package_name(test_line, 0)
         self.assertEqual("org.tarkleigh.foundation", package_name)
